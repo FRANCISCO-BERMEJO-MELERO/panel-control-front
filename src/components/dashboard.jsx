82142132393cbc0e5fba1a309c06117ad59c60ad
juan <jdeomoya@gmail.com>
2024-08-12 13:55:23 +0200

    añadido el componente de la barra lateral del dashboard e integrado en astro

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
 function Dashboard() {
-  return <p>hola</p>;
+  return (
+    <div className="flex flex-col bg-[#0f0c29] w-64 h-screen">
+      <div className="p-4 text-white p-4 hover:bg-[#252849]">Usuarios</div>
+      <div className="p-4 text-white p-4 hover:bg-[#252849]">
+        Modificar usuarios
+      </div>
+      <div className="p-4 text-white p-4 hover:bg-[#252849]">
+        Modificar usuarios
+      </div>
+      <div className="p-4 text-white p-4 hover:bg-[#252849]">
+        Modificar usuarios
+      </div>
+      <div className="p-4 text-white p-4 hover:bg-[#252849]">
+        Modificar usuarios
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
