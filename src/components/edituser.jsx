cb32c4893225ccdb1caaa39c8a238ceec31fed22
juan <jdeomoya@gmail.com>
2024-08-20 17:44:19 +0200

    probando estilos que funcionen. A la espera de nuevos estilos

--- src/components/edituser.jsx
+++ src/components/edituser.jsx
@@ -1,14 +1,22 @@
 import User from "./user";
+import "./styles/styles.css";
 
-function Edituser({ nombre, apellidos }) {
+function Edituser({ nombre, apellidos, id }) {
   return (
     <div className="flex flex-col bg-[#ffff] w-screen h-screen">
+      <div className="flex flex-row bg-[19a689] p-4 border-2 border-black border-solid hover:bg-slate-600 ease-in duration-150 text-center">
+        <button className="bg-[#19a689] p-2 border-solid rounded text-white hover:bg-slate-600 ease-in duration-150">
+          <i class="fa-solid fa-plus"></i> Add
+        </button>
+      </div>
       <div className="flex-1 p-4 overflow-auto scrollbar-thin scrollbar-thumb-slate-700 scrollbar-track-slate-300">
         {nombre.map((nombres, index) => (
-          <User key={index} nombre={nombres} apellidos={apellidos[index]} />
-        ))}
-        {nombre.map((nombres, index) => (
-          <User key={index} nombre={nombres} apellidos={apellidos[index]} />
+          <User
+            key={id[index]}
+            nombre={nombres}
+            apellidos={apellidos[index]}
+            id={index}
+          />
         ))}
       </div>
     </div>
